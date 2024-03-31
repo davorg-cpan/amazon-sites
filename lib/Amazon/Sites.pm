3f263a433088a81b63d58f2827a33a8ab3aeef89
Dave Cross <dave@dave.org.uk>
2024-03-31 15:08:12 +0100

    Fix MANIFEST.SKIP and MANIFEST. Prep release 0.1.4

--- lib/Amazon/Sites.pm
+++ lib/Amazon/Sites.pm
@@ -36,7 +36,7 @@ use Feature::Compat::Class;
 use feature 'signatures';
 no warnings 'experimental::signatures';
 
-our $VERSION = '0.1.3';
+our $VERSION = '0.1.4';
 
 class Amazon::Sites {
   use Amazon::Site;
